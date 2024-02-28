7932fd5f7d1a205dbd6d71a51bdc3f8944e7ed89
William Wun <williamwun@gmail.com>
2024-02-28 10:20:41 -0800

    save(), update(), delete() finished v132

--- src/main/java/com/william/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/william/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -1,14 +1,19 @@
 package com.william.curso.springboot.jpa.springbootjpa;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.william.curso.springboot.jpa.springbootjpa.entities.Person;
 import com.william.curso.springboot.jpa.springbootjpa.repositories.PersonRepository;
+
+
 
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner{
@@ -23,12 +28,129 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		update();
+
+	}
+
+	@Transactional
+	public void delete2(){
+
+		repository.findAll().forEach(System.out::println);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingresar el ID de la persona");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+
+		// if(optionalPerson.isPresent()){
+		// 	repository.deleteById(id);
+		// }
+
+		//usando ifPresentOrElse
+		optionalPerson.ifPresentOrElse(repository::delete, () -> System.out.println("No encontrado"));	//person -> repository.delete(person) = repository::delete
+
+		repository.findAll().forEach(System.out::println);
+
+		scanner.close();
+	}
+
+	@Transactional
+	public void delete(){
+
+		repository.findAll().forEach(System.out::println);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingresar el ID de la persona");
+		Long id = scanner.nextLong();
+
+		repository.deleteById(id);
+		
+		repository.findAll().forEach(System.out::println);
+
+		scanner.close();
+	}
+
+	@Transactional
+	public void update(){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingresar el ID de la persona: ");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+
+		//usando ifPresent
+		//optionalPerson.ifPresent(person -> {
+		//usando isPresent
+		if(optionalPerson.isPresent()){
+			Person personDb = optionalPerson.orElseThrow();
+			System.out.println(personDb);
+			System.out.println("Ingrese el lenguaje de programación");
+			String programmingLanguage = scanner.next();
+			personDb.setProgrammingLanguage(programmingLanguage);
+			Person personUpdated = repository.save(personDb);
+			System.out.println(personUpdated);
+		}else{
+			System.out.println("El usuario no existe");
+		}
+		//);
+
+		scanner.close();
+	}
+
+	@Transactional	//es un aspecto también
+	public void create(){
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingresar el nombre: ");
+		String name = scanner.next();
+		System.out.println("Ingresar el apellido: ");
+		String lastname = scanner.next();
+		System.out.println("Ingresar el lenguaje de programación: ");
+		String programmingLanguage = scanner.next();
+		scanner.close();
+		
+		Person person = new Person(null, name, lastname, programmingLanguage);
+
+		Person personNew = repository.save(person);	//save no es necesario agregarlo a Person Repository porque se está usando el nombre predefinido, en el caso de findByNameContaining lo que hace tabmién está predefinido pero no el nombre
+		System.out.println(personNew);
+
+		repository.findById(personNew.getId()).ifPresent(System.out::println);
+	}
+
+	@Transactional(readOnly = true)
+	public void findOne(){
+
+		// Person person = null;
+		// Optional<Person> optionalPerson = repository.findById(7L);	//findById devuelve optional
+		// //if(!optionalPerson.isEmpty()){
+		// if(optionalPerson.isPresent()){
+		// 	person = optionalPerson.get();
+		// }
+		// System.out.println(person);
+
+		//repository.findById(7L).ifPresent(System.out::println);	//repository.findById(7L).ifPresent(person -> System.out.println(person));	//person se pasa al print
+		//repository.findOne(7L).ifPresent(System.out::println);
+		//repository.findOneName("William").ifPresent(System.out::println);
+		//repository.findByName("William").ifPresent(System.out::println);
+		//repository.findOneLikeName("Will").ifPresent(System.out::println);
+		repository.findByNameContaining("Will").ifPresent(System.out::println);
+
+	}
+
+	@Transactional(readOnly = true)
+	public void List(){
 		//List<Person> persons = (List<Person>)repository.findAll();
 		//List<Person> persons = (List<Person>)repository.findByProgrammingLanguage("java");
 		//List<Person> persons = (List<Person>)repository.buscarByProgrammingLanguage("java");	//personalizado, hace lo mismo que findByProgrammingLanguage
 		List<Person> persons = (List<Person>)repository.buscarByProgrammingLanguageAndName("java", "William");	//ahora con dos parámetros
 
 		persons.stream().forEach(person -> {System.out.println(person);});
+
+		List<Object[]> personValues = repository.obtenerPersonaData("William");
+
+		personValues.stream().forEach(person -> {System.out.println(person[0]+ " es experto en : " + person[1]);});
 	}
 
 }
--- src/main/java/com/william/curso/springboot/jpa/springbootjpa/repositories/PersonRepository.java
+++ src/main/java/com/william/curso/springboot/jpa/springbootjpa/repositories/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.william.curso.springboot.jpa.springbootjpa.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,6 +10,19 @@ import com.william.curso.springboot.jpa.springbootjpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long>{ //<NombreDeClaseEntity, tipoDeDatoLlavePrimaria>
 
+    @Query("SELECT p FROM Person p where p.id =?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("SELECT p FROM Person p where p.name =?1")
+    Optional<Person> findOneName(String name);
+
+    Optional<Person> findByName(String name);
+
+    @Query("SELECT p FROM Person p where p.name LIKE %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+    Optional<Person> findByNameContaining(String name);
+
     List<Person> findByProgrammingLanguage(String programmingLanguage);
 
     @Query("SELECT p FROM Person p WHERE p.programmingLanguage=?1")    //es una consulta jpa(se usan los nombres de la clase y sus atributos), no sql (no el nombre de la tabla)    //=?1 número de argumento que se envía
@@ -16,4 +30,16 @@ public interface PersonRepository extends CrudRepository<Person, Long>{ //<Nombr
 
     @Query("SELECT p FROM Person p WHERE p.programmingLanguage=?1 AND name=?2")
     List<Person> buscarByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("SELECT p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p WHERE p.name=?1")
+    List<Object[]> obtenerPersonaData(String name);
+
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p WHERE p.programmingLanguage=?1 AND p.name=?2")
+    List<Object[]> obtenerPersonaData(String programmingLanguage, String name);
+
+    @Query("SELECT p.name, p.programmingLanguage FROM Person p WHERE p.programmingLanguage=?1")
+    List<Object[]> obtenerPersonDataByProgrammingLanguage(String programmingLanguage);
 }
